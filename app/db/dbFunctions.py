f4b716a6b09c7b6ba96018a60a3c47953d34bfdd
araalifarooq <farooqp35h@gmail.com>
2018-08-30 16:08:00 +0300

    Add update or accept answer module [Finishes #160128645]

--- app/db/dbFunctions.py
+++ app/db/dbFunctions.py
@@ -105,11 +105,59 @@ def get_question_by_id(qstn_id):
         return True
     return False
 
+def get_answer_by_id(ans_id):
+    # check if answer exists.
+    query = ("""SELECT * FROM answers where ans_id = '{}'""".format(ans_id))
+    cursor.execute(query)
+    answer = cursor.fetchone()
+    if answer:
+        return True
+    return False    
+
 def post_new_answer(answer, ans_owner, qstn_id, vote, status, date):
     #post a new answer
     query = (
         """INSERT INTO answers (answer, ans_owner, qstn_id, votes, status, date) VALUES ('{}', '{}', '{}','{}', '{}', '{}')""".format(answer, ans_owner, qstn_id, vote, status, date))
     cursor.execute(query)
+
+
+def update_answer(answer, ans_id, qstn_id):
+    """function to update answer"""
+    try:
+        query = ("""UPDATE answers SET answer = '{}' where ans_id = '{}' and qstn_id = '{}'""" .format(
+            answer, ans_id, qstn_id))
+        cursor.execute(query)
+        count = cursor.rowcount
+        if int(count) > 0:
+            return jsonify({"message":"Answer successfully updated"}),200
+        else:
+            return jsonify({"message":"Answer not updated, or doesn't exist"}),400   
+
+    except Exception as exception:
+        return jsonify({"message":str(exception)}),400   
+
+def accept_answer(status, qstn_id, ans_id):
+    """function to accept or reject answer"""
+    try:
+        query = ("""UPDATE answers SET status = '{}' where ans_id = '{}' and qstn_id = '{}'""" .format(
+            status, ans_id, qstn_id))
+        cursor.execute(query)
+        count = cursor.rowcount
+        if int(count) > 0:
+            return jsonify({"message":"Answer successfully accepted"}),200
+        else:
+            return jsonify({"message":"Failed to accept answer, or it doesn't exist"}),400   
+
+    except Exception as exception:
+        return jsonify({"message":str(exception)}),400   
+
+
+def get_answer_details(qstn_id, ans_id):
+    """ function to get details of a question"""
+    cursor.execute("""SELECT * FROM answers WHERE qstn_id = '{}' and ans_id = '{}'""".format(qstn_id, ans_id))
+    row = cursor.fetchone()
+    return row
+
 
 
 
